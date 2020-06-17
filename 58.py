0356266cf2351bec5cdbf2d2e82d41a7c40f57ac
Ann <983193453@qq.com>
2020-06-17 09:35:30 +0800

    first

--- 58.py
+++ 58.py
@@ -1,9 +1,9 @@
 import base64
-
 import requests,os,time
 from lxml import etree
 def down(url):
     detailresult=etree.HTML(requests.get(url,headers=headers).text)
+    print(detailresult.text.split("base64,")[1].split("'")[0].strip())
     money=detailresult.xpath('//div[@class="house-basic-right fr"]//div[@class="house-pay-way f16"]//b[@class="f36 strongbox"]/text()')
     base64_string = detailresult.text.split("base64,")[1].split("'")[0].strip()
     # 将 base64 编码的字体字符串解码成二进制编码
@@ -17,6 +17,7 @@ headers={
 url='https://hz.58.com/chuzu/?PGTID=0d100000-0004-fa1d-33d1-dec32394de7d&ClickID=2'
 result=etree.HTML(requests.get(url,headers=headers).text)
 fangzi=result.xpath('//div[@class="list-wrap"]//div[@class="list-box"]/ul/li')[0]
+print(fangzi.xpath('./div[@class="des"]/h2/a/@href')[0])
 # for i in fangzi:
 down(fangzi.xpath('./div[@class="des"]/h2/a/@href')[0])
 
